93d6b8facd34a77a4a6264acd0b17d629eb7603a
SweetLove1643 <phanuan028@gmail.com>
2024-11-22 00:11:27 +0700

    nothing

--- src/main/java/vn/project/Controllers/CustomerControllers/CartController.java
+++ src/main/java/vn/project/Controllers/CustomerControllers/CartController.java
@@ -1,9 +1,8 @@
 package vn.project.Controllers.CustomerControllers;
 
-import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import vn.project.Service.ICartService;
@@ -16,14 +15,7 @@ public class CartController {
     private ICartService cartService;
 
     @GetMapping
-    public String index(HttpSession session, Model model) {
-        Integer userId = (Integer) session.getAttribute("userId");
-
-        if (userId == null) {
-            return "commons/login";
-        }
-
-        model.addAttribute("carts", cartService.findByUser(userId));
+    public String index() {
         return "customer/cart";
     }
 }
