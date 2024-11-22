c7ce76fa5066fe273bb021950409ebef65e2e90c
NGUYEN-TRINH-QU\asus <quanganhpv19@gmail.com>
2024-11-23 06:01:17 +0700

    thêm order, personal data,change password,addresses

---
+++ src/main/java/vn/project/Controllers/CustomerControllers/changePasswordController.java
@@ -0,0 +1,16 @@
+package vn.project.Controllers.CustomerControllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/changePassword")
+public class changePasswordController {
+
+    @GetMapping
+
+    public String index() {
+        return "customer/changePassword";
+    }
+}
