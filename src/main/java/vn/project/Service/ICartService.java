681bccc5526c58b06eab96f3b712656957c4ff28
SweetLove1643 <phanuan028@gmail.com>
2024-11-23 23:45:28 +0700

    fix cart service,...

--- src/main/java/vn/project/Service/ICartService.java
+++ src/main/java/vn/project/Service/ICartService.java
@@ -1,6 +1,22 @@
 package vn.project.Service;
 
+import java.util.List;
+
+import vn.project.DTO.CartDTO;
+import vn.project.Entity.Cart;
 
 public interface ICartService {
+
+	List<CartDTO> ConvertCartToCartDTO(List<Cart> carts);
+
+	long count();
+
+	<S extends Cart> S save(S entity);
+
+	void deleteByUseridAndProductid(int userid, int productid);
+
+	void deleteByUserid(int userid);
+
+	List<CartDTO> findByUserid(int userid);
 
 }
--- src/main/java/vn/project/Service/Impl/CartService.java
+++ src/main/java/vn/project/Service/Impl/CartService.java
@@ -1,9 +1,8 @@
 package vn.project.Service.Impl;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,67 +10,83 @@ import org.springframework.stereotype.Service;
 import vn.project.DTO.CartDTO;
 import vn.project.DTO.ProductsDTO;
 import vn.project.Entity.Cart;
-import vn.project.Entity.Products;
 import vn.project.Repository.ICartRepository;
-import vn.project.Repository.IProductRepository;
-import vn.project.Repository.IUserRepository;
 import vn.project.Service.ICartService;
 import vn.project.Service.IProductService;
 
 @Service
-public class CartService implements ICartService{
-	
+public class CartService implements ICartService {
+
 	@Autowired
 	ICartRepository cartRepository;
-	
+
 	@Autowired
 	IProductService productService;
 
+	@Override
 	public List<CartDTO> findByUserid(int userid) {
-		List<Cart> usercart = cartRepository.findByUserid(userid);
-		
-		return null;
+		try {
+			List<Cart> usercart = cartRepository.findByUserid(userid);
+
+			return ConvertCartToCartDTO(usercart);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
+	@Override
 	public void deleteByUserid(int userid) {
 		cartRepository.deleteByUserid(userid);
 	}
 
+	@Override
 	public void deleteByUseridAndProductid(int userid, int productid) {
 		cartRepository.deleteByUseridAndProductid(userid, productid);
 	}
 
+	@Override
 	public <S extends Cart> S save(S entity) {
 		return cartRepository.save(entity);
 	}
 
+	@Override
 	public long count() {
 		return cartRepository.count();
 	}
 
-	public List<CartDTO> ConvertCartToCartDTO(List<Cart> carts){
-		List<CartDTO> cartdto = new ArrayList<>();
-		
-		for(Cart cart : carts) {
-			CartDTO cartdtopresent = new CartDTO();
-			cartdtopresent.setCartid(cart.getCartid());
-			cartdtopresent.setUserid(cart.getUserid());
-			cartdtopresent.setProductid(cart.getProductid());
-			
-			ProductsDTO product = productService.findbyIdDTO(cart.getProductid());
-			
-			cartdtopresent.setProductname(product.getProductname());
-			cartdtopresent.setCategory(product.getCategory());
-			cartdtopresent.setSupplier(product.getSupplier());
-			cartdtopresent.setBrand(product.getBrand());
-			cartdtopresent.setPrice(product.getPrice());
-			cartdtopresent.setDescription(product.getDescription());
-			cartdtopresent.setImageurl(product.getImageurl());
-			cartdtopresent.setQuantity(String.valueOf(cart.getQuantity()));
-			
-			cartdto.add(cartdtopresent);
+	@Override
+	public List<CartDTO> ConvertCartToCartDTO(List<Cart> carts) {
+		try {
+			List<CartDTO> cartdto = new ArrayList<>();
+
+			for (Cart cart : carts) {
+				CartDTO cartdtopresent = new CartDTO();
+				cartdtopresent.setCartid(cart.getCartid());
+				cartdtopresent.setUserid(cart.getUserid());
+				cartdtopresent.setProductid(cart.getProductid());
+
+				ProductsDTO product = productService.findbyIdDTO(cart.getProductid());
+
+				cartdtopresent.setProductname(product.getProductname());
+				cartdtopresent.setCategory(product.getCategory());
+				cartdtopresent.setSupplier(product.getSupplier());
+				cartdtopresent.setBrand(product.getBrand());
+				cartdtopresent.setPrice(product.getPrice());
+				cartdtopresent.setDescription(product.getDescription());
+				cartdtopresent.setImageurl(product.getImageurl());
+				cartdtopresent.setQuantity(String.valueOf(cart.getQuantity()));
+
+				cartdto.add(cartdtopresent);
+			}
+			return cartdto;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
 		}
-		return cartdto;
+
 	}
-	
+
 }
