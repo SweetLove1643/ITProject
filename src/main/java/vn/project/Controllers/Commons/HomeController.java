b878979d49c5495042853f92481420ad8e0cb11d
SweetLove1643 <phanuan028@gmail.com>
2024-11-27 03:32:59 +0700

    crud profile

--- src/main/java/vn/project/Controllers/Commons/HomeController.java
+++ src/main/java/vn/project/Controllers/Commons/HomeController.java
@@ -1,8 +1,4 @@
 package vn.project.Controllers.Commons;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +15,6 @@ public class HomeController{
 	
 	@GetMapping
 	public String index() {	
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		System.out.println(((UserDetails)auth.getPrincipal()).getAuthorities());
 		return "index";
 	}
 }
--- src/main/java/vn/project/Controllers/CustomerControllers/PersonalDataController.java
+++ src/main/java/vn/project/Controllers/CustomerControllers/PersonalDataController.java
@@ -2,22 +2,25 @@ package vn.project.Controllers.CustomerControllers;
 
 import java.util.ArrayList;
 
-
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 import vn.project.DTO.CartDTO;
 import vn.project.Entity.Orders;
-import vn.project.Entity.Roles;
 import vn.project.Entity.Users;
 import vn.project.Service.ICartService;
 import vn.project.Service.IOrderService;
@@ -36,74 +39,77 @@ public class PersonalDataController {
 
 	@Autowired
 	ICartService cartService;
-	
+
 	@Autowired
 	IOrderService orderService;
 
 	@GetMapping("/profile")
-	public String profile(HttpSession session, Model model) {
+	public String profile(Model model) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-	    Optional<Users> user = Optional.empty();
+		if (auth != null && auth.isAuthenticated() && !(auth.getPrincipal() instanceof String)) {
+			Object principal = auth.getPrincipal();
 
-	    Object useridObj = session.getAttribute("userid");
-	    if (useridObj != null) {
-	        String useridString = String.valueOf(useridObj).trim();
+			if (principal instanceof UserDetails) {
+				UserDetails userDetails = (UserDetails) principal;
+				Users user = userService.findByUsername(userDetails.getUsername()).orElse(null);
 
-	        if (!useridString.isEmpty() && !"null".equals(useridString)) {
-	            try {
-	                int userid = Integer.parseInt(useridString);
-	                user = userService.findById(userid);
-	            } catch (NumberFormatException e) {
-	                System.err.println("Invalid userid format in session: " + useridString);
-	            }
-	        }
-	    }
+				if (user != null) {
+					model.addAttribute("user", user);
+					return "customer/personaldata";
+				}
+			}
+		}
 
-	    if (user.isPresent()) {
-	        Users userPresent = user.get();
-	        String roleUser = "Không xác định";
-
-	        // Lấy role user nếu có
-	        Optional<Roles> role = roleService.findById(userPresent.getRole().getRoleid());
-	        if (role.isPresent()) {
-	            Roles rolePresent = role.get();
-	            roleUser = rolePresent.getRolename();
-	        }
-
-	        // Gửi dữ liệu về view
-	        model.addAttribute("user", userPresent);
-	        model.addAttribute("userrole", roleUser);
-	    } else {
-	        model.addAttribute("error", "User không tồn tại hoặc không được đăng nhập.");
-	    }
-
-	    return "customer/personaldata";
+		model.addAttribute("message", "Vui lòng đăng nhập");
+		return "redirect:/login";
 	}
 
+	@PostMapping("profile/update")
+	public String updateprofile(@ModelAttribute Users user, Model model) {
+		try {
+
+			Optional<Users> emailuser = userService.findByEmail(user.getEmail());
+
+			if (emailuser.isPresent() && user.getEmail().equals(emailuser.get().getEmail())) {
+				model.addAttribute("message", "Email đã tồn tại.");
+				return "redirect:/personal/profile";
+			}
+
+			user.setPassword(userService.findById(user.getId()).get().getPassword());
+
+			model.addAttribute("message", "Cập nhật thành công");
+			userService.save(user);
+			return "redirect:/personal/profile";
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "redirect:/404";
+		}
+
+	}
 
 	@GetMapping("/cart")
 	public String cart(HttpSession session, Model model) {
-		
+
 		List<CartDTO> cartuser = new ArrayList<>();
-		
+
 		cartuser = cartService.findByUserid(1);
 
-		
 		Object useridObj = session.getAttribute("userid");
-		if(useridObj != null) {
+		if (useridObj != null) {
 			String useridString = String.valueOf(useridObj).trim();
-			
-			if(!useridString.isEmpty() || !"null".equals(useridString)) {
+
+			if (!useridString.isEmpty() || !"null".equals(useridString)) {
 				try {
 					int userid = Integer.parseInt(useridString);
 					cartuser = cartService.findByUserid(userid);
-	            } catch (NumberFormatException e) {
-	                e.printStackTrace();
-	            }
+				} catch (NumberFormatException e) {
+					e.printStackTrace();
+				}
 			}
 		}
-		
-		
+
 		if (cartuser != null) {
 			model.addAttribute("usercart", cartuser);
 		} else {
@@ -112,11 +118,11 @@ public class PersonalDataController {
 
 		return "customer/cart";
 	}
-	
+
 	@Transactional
 	@GetMapping("/cart/delete/{id}")
 	public String deletecart(@PathVariable("id") int cartid) {
-		
+
 		cartService.deleteByCartid(cartid);
 
 		return "redirect:/personal/cart";
@@ -130,8 +136,7 @@ public class PersonalDataController {
 	@GetMapping("/orders")
 	public String orders(Model model) {
 		List<Orders> order = orderService.findByUserid(4);
-		
-		
+
 		model.addAttribute("orders", order);
 		return "customer/acc-orders";
 	}
