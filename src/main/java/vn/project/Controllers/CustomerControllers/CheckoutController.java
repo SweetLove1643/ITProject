6604b71b83e94b93b4f2c21e379b4d4914256a8b
SweetLove1643 <phanuan028@gmail.com>
2024-12-09 02:36:38 +0700

    checkout vnpay

--- src/main/java/vn/project/Controllers/CustomerControllers/CheckoutController.java
+++ src/main/java/vn/project/Controllers/CustomerControllers/CheckoutController.java
@@ -121,9 +121,14 @@ public class CheckoutController {
 	}
 
 	@PostMapping("/checkout")
-	public String applyVoucher(@RequestParam List<String> listcheckout, @RequestParam String voucher,
-			@RequestParam("totalamount") String tongtien, Model model, @RequestParam("submit") String submit,
-			@RequestParam("paymentmethod") String paymentmethod, HttpSession session, RedirectAttributes redirectAttributes) {
+	public String applyVoucher(@RequestParam List<String> listcheckout, 
+			@RequestParam String voucher,
+			@RequestParam("totalamount") String tongtien, 
+			Model model, 
+			@RequestParam("submit") String submit,
+			@RequestParam("paymentmethod") String paymentmethod, 
+			HttpSession session, 
+			RedirectAttributes redirectAttributes) {
 		try {
 
 			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -135,7 +140,7 @@ public class CheckoutController {
 				return "redirect:/exception/403";
 			}
 
-			if ("voucher".equals(submit)) {
+			if ("voucher".equals(submit)) { // xử lí áp voucher
 				List<ProductsDTO> productcheckout = new ArrayList<>();
 
 				for (String idproduct : listcheckout) {
@@ -175,56 +180,81 @@ public class CheckoutController {
 				model.addAttribute("usercart", productcheckout);
 
 				return "customer/checkout";
-			} else {
+			} else {// nguoc lại thi thanh toan
 				if ((Map<String, String>) session.getAttribute("quantities") instanceof Map<String, String>
 						&& listcheckout != null) {
 					Map<String, String> quantities = (Map<String, String>) session.getAttribute("quantities");
 
 					Optional<Discounts> discount = discountService.findByDiscountcode(voucher);
-					if (discount.isPresent()) {
+					if (discount.isPresent()) { // truong hợp có voucher thì them vô order
+						Long amount = Long.valueOf(tongtien);
+						
+						List<Order_Products> listorderproduct = new ArrayList<>(); // tao de luu session, insert khi da thanh toan
+						
+						Orders order = Orders.builder() // tạo đơn, luu session su dung sau thanh toans
+								.deliverystatus("Đang xử li") // Mac dinh là dang xư lí
+								.discountid(discount.get().getDiscountid()) // mac dinh
+								.orderdate(LocalDateTime.now()) // lấy time hienj tại
+								.paymentmethod(paymentmethod) 
+								.paymentstatus("Đã thanh toán")
+								.totalamount(amount)
+								.userid(user.getId())
+								.build();
+
+						for (String idproduct : listcheckout) {
+							int id = Integer.parseInt(idproduct);
+							int quantity = Integer.parseInt(quantities.get("quantity-" + idproduct)); // lay so luong theo id product
+							Products product = productService.findById(id).get();
+							Order_Products orderproduct = Order_Products.builder() // tao lien ket order
+									.order(order)
+									.product(product)
+									.quantity(quantity)
+									.build();
+							listorderproduct.add(orderproduct); // luu session
+//							cartService.deleteByUseridAndProductid(user.getId(), Integer.parseInt(idproduct));
+						}
+						
+						session.setAttribute("order", order); //
+						session.setAttribute("listorderproduct", listorderproduct);
+						redirectAttributes.addFlashAttribute("totalamount", amount); // gui so tien qua vnpay
+
+						session.removeAttribute("quantities");
+						
+						return "redirect:/personal/create_payment";
+
+					} else { // không có voucher thì bỏ id voucher
+						
+						List<Order_Products> listorderproduct = new ArrayList<>(); // tao de luu session, insert khi da thanh toan
+						
 						Long amount = Long.valueOf(tongtien);
 						Orders order = Orders.builder().deliverystatus("Đang xử li")
-								.discountid(discount.get().getDiscountid()).orderdate(LocalDateTime.now())
-								.paymentmethod(paymentmethod).paymentstatus("Đã thanh toán").totalamount(amount)
+								.orderdate(LocalDateTime.now())
+								.paymentmethod(paymentmethod)
+								.paymentstatus("Đã thanh toán")
+								.totalamount(amount)
 								.userid(user.getId()).build();
 
-						orderService.save(order);
-
-						for (String idproduct : listcheckout) {
-							int id = Integer.parseInt(idproduct);
-							int quantity = Integer.parseInt(quantities.get("quantity-" + idproduct));
-							Products product = productService.findById(id).get();
-							Order_Products orderproduct = Order_Products.builder().order(order).product(product)
-									.quantity(quantity).build();
-							orderproductService.save(orderproduct);
-							cartService.deleteByUseridAndProductid(user.getId(), Integer.parseInt(idproduct));
-						}
-
-						session.removeAttribute("quantities");
-						model.addAttribute("message", "Đặt hàng thành công");
-						return "customer/cart";
-
-					} else {
-						Long amount = Long.valueOf(tongtien);
-						Orders order = Orders.builder().deliverystatus("Đang xử li").orderdate(LocalDateTime.now())
-								.paymentmethod(paymentmethod).paymentstatus("Đã thanh toán").totalamount(amount)
-								.userid(user.getId()).build();
-
-						orderService.save(order);
+						
 
 						for (String idproduct : listcheckout) {
 							int quantity = Integer.parseInt(quantities.get("quantity-" + idproduct));
 							int id = Integer.parseInt(idproduct);
 							Products product = productService.findById(id).get();
-							Order_Products orderproduct = Order_Products.builder().order(order).product(product)
-									.quantity(quantity).build();
-							orderproductService.save(orderproduct);
-							cartService.deleteByUseridAndProductid(user.getId(), Integer.parseInt(idproduct));
+							Order_Products orderproduct = Order_Products.builder()
+									.order(order)
+									.product(product)
+									.quantity(quantity)
+									.build();
+							listorderproduct.add(orderproduct);
+//							cartService.deleteByUseridAndProductid(user.getId(), Integer.parseInt(idproduct));
 						}
 
-						redirectAttributes.addFlashAttribute("message", "Đặt hàng thành công");
+						session.setAttribute("order", order); //
+						session.setAttribute("listorderproduct", listorderproduct);
+						redirectAttributes.addFlashAttribute("totalamount", amount);
+						
 						session.removeAttribute("quantities");
-						return "redirect:/personal/cart";
+						return "redirect:/personal/create_payment";
 					}
 				} else {
 					return "redirect:/exception/anyerror";
